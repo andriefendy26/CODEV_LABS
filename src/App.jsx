3eb0c64a190083feb24295c25525f6c098f751d8
Andri Efendy <andri.nnkn@gmail.com>
2025-05-24 23:37:13 +0800

    fix bug on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
-import Routers from "./Route/index";
+import Routers from "./route/index";
 import "./App.css";
 import Aos from "aos";
-import 'aos/dist/aos.css'; 
+import "aos/dist/aos.css";
 
 function App() {
-   useEffect(() => {
+  useEffect(() => {
     Aos.init({
       duration: 1000, // durasi animasi dalam ms
-      once: true,     // hanya animasi satu kali
+      once: true, // hanya animasi satu kali
     });
   }, []);
   return (
